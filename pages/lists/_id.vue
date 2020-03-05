b3f10badf22abb3facea1d14fc3b50f3edcec49e
Marijn <oomensmarijn@gmail.com>
2020-03-05 21:55:32 +0100

    Update list page

--- pages/lists/_id.vue
+++ pages/lists/_id.vue
@@ -1,9 +1,17 @@
 <template>
-    <v-container fluid>
+<v-layout justify-center align-center>
+    <v-container justify-center align-center>
         <v-row justify="center">
             <h1>{{ list.title }}</h1>
         </v-row>
+        <v-row justify="center">
+            <p>Amount of words: {{ list.lang1.words.length }}</p>
+        </v-row>
+        <v-row justify="center">
+            <v-btn large color="primary">Start</v-btn>
+        </v-row>
     </v-container>
+</v-layout>
 </template>
 
 <script>
