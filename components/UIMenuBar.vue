8bfbb816ec7ba6cbd841c11881ea5b7c99eae512
Marijn <31892254+MarijnOomens@users.noreply.github.com>
2020-05-05 13:51:38 +0200

    manage page

--- components/UIMenuBar.vue
+++ components/UIMenuBar.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <v-app-bar dense >
-            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+            <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
             <v-toolbar-title><nuxt-link to="/">MarkMyWords</nuxt-link></v-toolbar-title>
             <v-spacer />
             <v-switch
@@ -11,8 +11,42 @@
                 label="Dark theme">
             </v-switch>
         </v-app-bar>
+        <v-navigation-drawer v-model="drawer" absolute temporary>
+            <v-list nav dense>
+                <v-list-item-group>
+                    <nuxt-link to="/">
+                    <v-list-item>
+                        <v-list-item-icon>
+                            <v-icon>mdi-home</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-title>Home</v-list-item-title>
+                    </v-list-item>
+                    </nuxt-link>
+
+                    <nuxt-link to="/manage">
+                    <v-list-item>
+                        <v-list-item-icon>
+                            <v-icon>mdi-view-list</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-title>Manage</v-list-item-title>
+                    </v-list-item>
+                    </nuxt-link>
+
+                </v-list-item-group>
+            </v-list>
+        </v-navigation-drawer>
     </div>
 </template>
+
+<script>
+export default {
+    data: () => {
+        return {
+            drawer: false
+        }
+    }
+}
+</script>
 
 <style>
 a, a:active, a:visited, a:hover {
